981bda097761d0f00a2e40f631de8b7644177e2c
Fernanda Carvalho Pires <fercarvalho91@gmail.com>
2025-11-07 16:41:34 +0100

    primeiro commit:estrutura inicial do portfólio

---
+++ script.js
@@ -0,0 +1,8 @@
+// menu smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(link=>{
+  link.addEventListener('click', e=>{
+    e.preventDefault();
+    const target = document.querySelector(link.getAttribute('href'));
+    if(target) target.scrollIntoView({behavior:'smooth', block: 'start'});
+  });
+});
